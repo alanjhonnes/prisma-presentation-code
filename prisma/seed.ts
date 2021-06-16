1a9b7c3245a9f765c72d28f8a6b802e68146e220
Alan Jhonnes <alanjhonnes@gmail.com>
2021-06-16 18:08:44 -0300

    feat: add more examples and fix seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,58 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { Post, PrismaClient, User } from '@prisma/client'
+const prisma = new PrismaClient({
+    log: ['error', 'info', 'query', 'warn']
+})
 async function seed() {
 
-    const users = await prisma.user.createMany({
-        data: [
-            {
-                name: 'alan',
-                email: 'alan@gmail.com'
-            },
-            {
-                name: 'alan2',
-                email: 'alan2@gmail.com'
-            },
-        ]
-    });
 
-    
+    const users: User[] = [];
+
+    for(let i = 0; i < 10; i++) {
+        const user = await prisma.user.create({
+            data: {
+                name: `User ${i}`,
+                email: `user${i}@mjv.com.br`,
+            },
+        });
+
+        users.push(user);
+    }
+
+    const posts: Post[] = [];
+
+    for(let i = 0; i < 3; i++) {
+        const post = await prisma.post.create({
+            data: {
+                title: `Post ${i}`,
+                authorId: users[i].id,
+                content: `Content ${i}`,
+                comments: {
+                    createMany: {
+                        data: [
+                            {
+                                authorId: users[i].id,
+                                comment: `Comment 1 Post ${i}`,
+                            },
+                            {
+                                authorId: users[i].id,
+                                comment: `Comment 2 Post ${i}`,
+                            },
+                        ]
+                    }
+                }
+            }
+        });
+
+        posts.push(post);
+    }
 }
 
-export default seed;
+seed()
+    .then(() => {
+        console.log('seeded db');
+        process.exit(0);
+    })
+    .catch((error) => {
+        console.error(error);
+        process.exit(0);
+    });
